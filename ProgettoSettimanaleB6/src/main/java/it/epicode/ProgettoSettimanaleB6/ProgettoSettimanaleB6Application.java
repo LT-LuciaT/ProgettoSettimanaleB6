d4c1b21198483bc9495044cb90a45d607b832da2
LT-LuciaT <lucia.trombin98@gmail.com>
2025-04-18 10:29:10 +0200

    1

---
+++ ProgettoSettimanaleB6/src/main/java/it/epicode/ProgettoSettimanaleB6/ProgettoSettimanaleB6Application.java
@@ -0,0 +1,13 @@
+package it.epicode.ProgettoSettimanaleB6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleB6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleB6Application.class, args);
+	}
+
+}
---
+++ ProgettoSettimanaleB6/src/test/java/it/epicode/ProgettoSettimanaleB6/ProgettoSettimanaleB6ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.ProgettoSettimanaleB6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleB6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
